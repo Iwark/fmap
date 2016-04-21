f7b49e6fca85c65395a62b97eb7011d889e7230b
Iwark <iwark02@gmail.com>
2016-04-21 20:23:14 +0900

    Enable converting time.Time

--- fmap.go
+++ fmap.go
@@ -1,23 +1,53 @@
 package fmap
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 	"strconv"
-	"strings"
+	"time"
+
+	"github.com/huandu/xstrings"
 )
+
+var timeFormats = []string{
+	"1/2/2006",
+	"1/2/2006 15:4:5",
+	"2006-1-2 15:4:5",
+	"2006-1-2 15:4",
+	"2006-1-2",
+	"1-2",
+	"15:4:5",
+	"15:4",
+	"15",
+	"15:4:5 Jan 2, 2006 MST",
+}
+
+// const (
+// 	// SnakeCase uses snake_case to convert
+// 	SnakeCase = iota
+// )
+
+// // Case represents the case of form names
+// var Case = SnakeCase
 
 // ConvertToStruct converts req.Form to the struct which has the json tags
 func ConvertToStruct(m map[string][]string, s interface{}) error {
 
 	tagInfo := make(map[string]string)
 	rt := reflect.TypeOf(s).Elem()
+	sName := xstrings.ToSnakeCase(rt.Name())
 	for i := 0; i < rt.NumField(); i++ {
+		var key string
 		if val := rt.Field(i).Tag.Get("fmap"); val != "" {
-			sName := strings.ToLower(rt.Name())
-			key := fmt.Sprintf("%s[%s]", sName, val)
-			tagInfo[key] = rt.Field(i).Name
+			if val == "-" {
+				continue
+			}
+			key = fmt.Sprintf("%s[%s]", sName, val)
+		} else {
+			key = fmt.Sprintf("%s[%s]", sName, xstrings.ToSnakeCase(rt.Field(i).Name))
 		}
+		tagInfo[key] = rt.Field(i).Name
 	}
 	for k, v := range m {
 		var err error
@@ -36,30 +66,51 @@ func ConvertToStruct(m map[string][]string, s interface{}) error {
 }
 
 func setField(obj interface{}, name string, value interface{}) error {
-	rv := reflect.ValueOf(obj).Elem().FieldByName(name)
-
+	rv := reflect.Indirect(reflect.ValueOf(obj)).FieldByName(name)
 	if !rv.IsValid() {
 		// No such field => SKIP
 		return nil
 	}
-
 	if !rv.CanSet() {
 		return fmt.Errorf("Cannot set %s field value", name)
 	}
 
 	val := reflect.ValueOf(value)
-	switch rv.Type().Name() {
-	case "int":
+	switch rv.Type() {
+	case reflect.TypeOf(int(0)):
 		intVal, err := strconv.Atoi(val.Interface().(string))
 		if err != nil {
 			return err
 		}
 		rv.Set(reflect.ValueOf(intVal))
-	case "string":
+	case reflect.TypeOf(""):
 		rv.Set(val)
+	case reflect.TypeOf(time.Time{}):
+		t, err := parseTime(val.String())
+		if err != nil {
+			return err
+		}
+		rv.Set(reflect.ValueOf(t))
+	case reflect.TypeOf(&time.Time{}):
+		t, err := parseTime(val.String())
+		if err != nil {
+			return err
+		}
+		rv.Set(reflect.ValueOf(&t))
 	default:
 		return fmt.Errorf("Provided value type didn't match obj field type val_type: %v, rv_type: %v", val.Type(), rv.Type())
 	}
 
 	return nil
+}
+
+func parseTime(str string) (t time.Time, err error) {
+	for _, format := range timeFormats {
+		t, err = time.Parse(format, str)
+		if err == nil {
+			return
+		}
+	}
+	err = errors.New("Can't parse string as time: " + str)
+	return
 }
--- fmap_test.go
+++ fmap_test.go
@@ -1,28 +1,44 @@
 package fmap
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 type Person struct {
-	Name   string
-	Age    int    `fmap:"age"`
-	Gender string `fmap:"gender"`
+	UpdatedAt time.Time
+	DeletedAt *time.Time
+	Name      string
+	Age       int    `fmap:"great_age"`
+	Gender    string `fmap:"gender"`
+	Birthday  time.Time
 }
 
 func TestConvert(t *testing.T) {
+	assert := assert.New(t)
+
 	formValue := map[string][]string{
-		"person[name]":   []string{"Iwark"},
-		"person[age]":    []string{"24"},
-		"person[gender]": []string{"man", "woman"},
-		"person[hobby]":  []string{"playing the piano"},
-		"not_person":     []string{"hoge", "fuga"},
+		"person[updated_at]": []string{"2016-04-13 15:24"},
+		"person[deleted_at]": []string{"2016-04-13"},
+		"person[name]":       []string{"Iwark"},
+		"person[great_age]":  []string{"24"},
+		"person[gender]":     []string{"man", "woman"},
+		"person[hobby]":      []string{"playing the piano"},
+		"not_person":         []string{"hoge", "fuga"},
 	}
 
 	result := &Person{}
 	err := ConvertToStruct(formValue, result)
-	if err != nil {
-		t.Error("Convert Error:", err)
-	}
-	if result.Name != "" || result.Age != 24 || result.Gender != "man" {
-		t.Error("Map to struct failed, got:", result)
-	}
+	assert.NoError(err)
+	require.NotNil(t, result.DeletedAt)
+	deleted := result.DeletedAt.Format("2006-01-02")
+	assert.Equal("2016-04-13", deleted)
+	updated := result.UpdatedAt.Format("2006-01-02 15:04")
+	assert.Equal("2016-04-13 15:24", updated)
+	assert.Equal("Iwark", result.Name)
+	assert.Equal(24, result.Age)
+	assert.Equal("man", result.Gender)
 }
